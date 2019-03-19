da7a4015c9f61ff430412b8a630ab7a7cb04046e
mumu-is-lzy <852389019@qq.com>
2019-03-19 10:00:15 +0800

    第四节 运用函数显示tf版本号

--- tf1/1.py
+++ tf1/1.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
 
-matrix1 = tf.constant([[3., 3.]])
-matrix2 = tf.constant([[2.],[2.]])
-product = tf.matmul(matrix1, matrix2)
-
 sess = tf.Session()
-result = sess.run(product)
-print (result)
+
+a = tf.placeholder("float")
+b = tf.placeholder("float")
+c = tf.constant(6.0)
+d = tf.multiply(a, b)
+y = tf.multiply(d, c)
+print(sess.run(y, feed_dict={a: 3, b: 3}))
+
+A = [[1.1, 2.3], [3.4, 4.1]]
+Y = tf.matrix_inverse(A)
+print(sess.run(Y))
 '''
 ---------------------
-
-
+作者：拾柴者
+来源：CSDN
+原文：https: // blog.csdn.net / qq_38404440 / article / details / 79893454
+版权声明：本文为博主原创文章，转载请附上博文链接！
 作者：懒散的木木酱
 来源：CSDN
 原文：https://blog.csdn.net/weixin_44350307/article/details/87776080
